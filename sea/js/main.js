19017d49453daa9ae5c30321eb41fbc26f4432fd
mayu9623-art <mayu9623@gmail.com>
2026-07-08 22:50:00 +0900

    Add files via upload

---
+++ sea/js/main.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+/*jshint strict:false*/
+$(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+});
