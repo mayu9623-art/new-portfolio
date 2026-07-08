106cd491cd8f1b64cb270afc19725c51c87ee231
mayu9623-art <mayu9623@gmail.com>
2026-07-08 22:43:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(function(){
+  $(".inview_re").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    } else {
+      $(this).stop().removeClass("is-show");
+    }
+  });
+});
